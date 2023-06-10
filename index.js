12b5f4c41c4aa317b8aa7a72678d3c95ff8f2f1c
banku27 <pankajparihar123321@gmail.com>
2023-06-11 01:06:52 +0530

    apis working successfully

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const dotenv= require('dotenv');
 const mongoose = require('mongoose')
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 // get variables from .env file || process.env.VARIABLE_NAME
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log('Database connected')).catch((err)=>{console.log(err)});
 
-app.get('/', (req, res) => res.send('Hello Pankaj!'))
+// app.get('/', (req, res) => res.send('Hello Pankaj!'))
+app.use(express.json());
+app.use("/api/",authRoute);
 
 app.listen(process.env.PORT||5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
-// here 5002 is an alternative if 5001 does not work then 5002 port will be started.
+// here 5002 is an alternative if 5001 does not work then 5002 port will be started. 
