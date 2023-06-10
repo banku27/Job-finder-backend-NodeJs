585e70c0807f1ce9b54b7e83f0758a40bcb47b84
banku27 <pankajparihar123321@gmail.com>
2023-06-10 17:06:58 +0530

    Project setup and database connection

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const dotenv= require('dotenv');
+const mongoose = require('mongoose')
+
+dotenv.config();
+// get variables from .env file || process.env.VARIABLE_NAME
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log('Database connected')).catch((err)=>{console.log(err)});
+
+app.get('/', (req, res) => res.send('Hello Pankaj!'))
+
+app.listen(process.env.PORT||5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+// here 5002 is an alternative if 5001 does not work then 5002 port will be started.
