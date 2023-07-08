f7d5827ede3fb434df8ea9cf7a1b0b6f172391cc
banku27 <pankajparihar123321@gmail.com>
2023-07-08 16:56:52 +0530

    routing and index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const jobRoute = require("./routes/job")
 const bookmarkRoute = require("./routes/bookmark")
+const chatRoute = require("./routes/chat")
+const messageRoute = require("./routes/message")
 
 dotenv.config();
 // get variables from .env file || process.env.VARIABLE_NAME
@@ -18,6 +20,8 @@ app.use("/api/",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/jobs",jobRoute);
 app.use("/api/bookmarks",bookmarkRoute);
+app.use("/api/chats",chatRoute);
+app.use("/api/messages",messageRoute);
 
 app.listen(process.env.PORT||5002, () => console.log(`Example app listening on port ${process.env.PORT}!`))
 // here 5002 is an alternative if 5001 does not work then 5002 port will be started. 
